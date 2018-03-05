4435b5f08e542ffc666c2ffc4a9303b1f50e01d4
Tyler Kinsherf <tyler.kinsherf@gmail.com>
2018-03-05 13:30:45 -0500

    Add project files.

---
+++ WebApplication1/Controllers/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Random()
+        {
+            var game = new Game() { Name = "Caverna" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Joe"},
+                new Customer {Name = "Steve"}
+            };
+
+            var viewModel = new RandomGameViewModel
+            {
+                Game = game,
+                Customer = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("game/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/RandomGameViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class RandomGameViewModel
+    {
+        public Game Game { get; set; }
+        public List<Customer> Customer { get; set; }
+    }
+}
